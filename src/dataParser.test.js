f3f6168f3cb943de49021ed7c775430a77e8f319
ryan p <rjp-coder@gmail.com>
2025-05-12 11:24:39 +0100

    refactor yearmonth removal

--- src/dataParser.test.js
+++ src/dataParser.test.js
@@ -1,54 +1,107 @@
-import { parseData, getPrecisePercentile } from "./dataParser"; // Adjust the import path as necessary
+import { removeYearMonth } from "./dataParser"; // Adjust the import path as necessary
 
-describe("dataParser", () => {
-  it("should parse data correctly", () => {
-    const data = `Year: Month Month L M S 1st 3rd 5th 15th 25th 50th 75th 85th 95th 97th 99th
-5: 1 61 -0.4681 18.2579 0.14295 13.4 14.2 14.6 15.8 16.6 18.3 20.2 21.3 23.4 24.3 26.2`;
-    const expected = {
-      lifeInMonths: 61,
-      L: -0.4681,
-      M: 18.2579,
-      S: 0.14295,
-      percentiles: [
-        13.4, 14.2, 14.6, 15.8, 16.6, 18.3, 20.2, 21.3, 23.4, 24.3, 26.2,
-      ],
-    };
-    const result = parseData(data, true, 4)[0]; // Assuming parseData returns an array
-    expect(result).toEqual(expected);
-  });
+// describe("dataParser", () => {
+//   it("should parse data correctly", () => {
+//     const data = `Year: Month Month L M S 1st 3rd 5th 15th 25th 50th 75th 85th 95th 97th 99th
+// 5: 1 61 -0.4681 18.2579 0.14295 13.4 14.2 14.6 15.8 16.6 18.3 20.2 21.3 23.4 24.3 26.2`;
+//     const expected = {
+//       lifeInMonths: 61,
+//       L: -0.4681,
+//       M: 18.2579,
+//       S: 0.14295,
+//       percentiles: [
+//         13.4, 14.2, 14.6, 15.8, 16.6, 18.3, 20.2, 21.3, 23.4, 24.3, 26.2,
+//       ],
+//     };
+//     const result = parseData(data, true, 4)[0]; // Assuming parseData returns an array
+//     expect(result).toEqual(expected);
+//   });
+//   it.each([
+//     /*the 5th percentile is 10, the 10th percentile is 15,
+//     that's one kg per percentile, 8kg is the 13th percentile */
+//     [5, 10, 10, 15, 13, 8],
+//     /*the 50th percentile is 20, the 75th percentile is 30, that's .4kg per percentile,
+//     26kg = 20kg + ( 0.4kg * 15) .
+//     50 + 15 is 65.*/
+//     [50, 20, 75, 30, 26, 65],
+//     /* similarly 28kg is (0.4kg * 20)
+//     50 + 20 is 70.*/
+//     [50, 20, 75, 30, 28, 70],
+//     /* But for 29kg, (0.4kg * 22 = 28.8kg or  (0.4kg * 23 = 29.2kg) could be the answer. The function
+//     picks the nearest one, or if equidistant, the higher one, which in this case is 23.
+//     50+23  = 73*/
+//     [50, 20, 75, 30, 29, 73],
+//   ])(
+//     "should be able to get the precise percentile for a given weight",
+//     (
+//       lowerPercentile,
+//       lowerWeight,
+//       upperPercentile,
+//       upperWeight,
+//       weight,
+//       expected
+//     ) => {
+//       const percentile = getPrecisePercentile(
+//         lowerPercentile,
+//         lowerWeight,
+//         upperPercentile,
+//         upperWeight,
+//         weight
+//       ); // Implement this function to calculate the percentile based on the parsed data and weight
+//       expect(percentile).toEqual(expected); // Adjust the expected value based on your logic
+//     }
+//   );
+// });
+
+describe("removeYearMonth", () => {
+  const girlsWeightAge5to10 = `Year: Month Month L M S 1st 3rd 5th 15th 25th 50th 75th 85th 95th 97th 99th 
+5: 1 61 -0.2026 18.5057 0.12988 13.8 14.6 15.0 16.2 17.0 18.5 20.2 21.2 23.0 23.8 25.3
+5: 2 62 -0.2130 18.6802 0.13028 13.9 14.7 15.1 16.4 17.1 18.7 20.4 21.4 23.3 24.0 25.6
+`;
+  const expectedGirlsWeightAge5to10 = `Month L M S 1st 3rd 5th 15th 25th 50th 75th 85th 95th 97th 99th 
+61 -0.2026 18.5057 0.12988 13.8 14.6 15.0 16.2 17.0 18.5 20.2 21.2 23.0 23.8 25.3
+62 -0.2130 18.6802 0.13028 13.9 14.7 15.1 16.4 17.1 18.7 20.4 21.4 23.3 24.0 25.6
+`;
+
+  const twoDigitMonth = `Year: Month Month L M S 
+5: 1 61 -0.2026 18.5057 0.12988 
+5:11 62 -0.2130 18.6802 0.13028 
+`;
+
+  const expectedTwoDigitMonth = `Month L M S 
+61 -0.2026 18.5057 0.12988 
+62 -0.2130 18.6802 0.13028 
+`;
+
+  const twoDigitYear = `Year: Month Month L M S 
+5: 1 61 -0.2026 18.5057 0.12988 
+10: 1 62 -0.2130 18.6802 0.13028 
+`;
+
+  const expectedTwoDigitYear = `Month L M S 
+61 -0.2026 18.5057 0.12988 
+62 -0.2130 18.6802 0.13028 
+`;
+
+  const twoDigitMonthYear = `Year: Month Month L M S 
+15:10 61 -0.2026 18.5057 0.12988 
+12:11 62 -0.2130 18.6802 0.13028 `;
+
+  const expectedTwoDigitMonthYear = `Month L M S 
+61 -0.2026 18.5057 0.12988 
+62 -0.2130 18.6802 0.13028 `;
+
   it.each([
-    /*the 5th percentile is 10, the 10th percentile is 15, 
-    that's one kg per percentile, 8kg is the 13th percentile */
-    [5, 10, 10, 15, 13, 8],
-    /*the 50th percentile is 20, the 75th percentile is 30, that's .4kg per percentile, 
-    26kg = 20kg + ( 0.4kg * 15) . 
-    50 + 15 is 65.*/
-    [50, 20, 75, 30, 26, 65],
-    /* similarly 28kg is (0.4kg * 20) 
-    50 + 20 is 70.*/
-    [50, 20, 75, 30, 28, 70],
-    /* But for 29kg, (0.4kg * 22 = 28.8kg or  (0.4kg * 23 = 29.2kg) could be the answer. The function
-    picks the nearest one, or if equidistant, the higher one, which in this case is 23.
-    50+23  = 73*/
-    [50, 20, 75, 30, 29, 73],
-  ])(
-    "should be able to get the precise percentile for a given weight",
-    (
-      lowerPercentile,
-      lowerWeight,
-      upperPercentile,
-      upperWeight,
-      weight,
-      expected
-    ) => {
-      const percentile = getPrecisePercentile(
-        lowerPercentile,
-        lowerWeight,
-        upperPercentile,
-        upperWeight,
-        weight
-      ); // Implement this function to calculate the percentile based on the parsed data and weight
-      expect(percentile).toEqual(expected); // Adjust the expected value based on your logic
-    }
-  );
+    [girlsWeightAge5to10, expectedGirlsWeightAge5to10],
+    [twoDigitMonth, expectedTwoDigitMonth],
+    [twoDigitYear, expectedTwoDigitYear],
+    [twoDigitMonthYear, expectedTwoDigitMonthYear],
+  ])("should remove the year month from input", (input, expected) => {
+    console.log("input", JSON.stringify(input));
+    console.log("output", JSON.stringify(removeYearMonth(input)));
+    console.log("expected", JSON.stringify(expected));
+    expect(JSON.stringify(removeYearMonth(input))).toEqual(
+      JSON.stringify(expected)
+    );
+  });
 });
--- src/dataParser.ts
+++ src/dataParser.ts
@@ -6,11 +6,71 @@ type DataRow = {
   percentiles: number[]; // Array of percentiles
 };
 
+export function removeColumns(array, indexes) {
+  const parts = [];
+  indexes.forEach((columnIndexToIgnore, i) => {
+    if (columnIndexToIgnore < 0) {
+      // if the index is negative, this allows it to count down from the right
+      // just like in the array.at() function.
+      indexes[i] += array.length;
+    }
+  });
+  for (let j = 0; j < array.length; j++) {
+    if (indexes.includes(j) || indexes.includes(j + array.length)) {
+      continue; // Skip ignored columns
+    }
+    const part = array[j];
+    parts.push(part);
+  }
+  return parts;
+}
+
+export function removeYearMonth(rawData) {
+  if (!rawData) {
+    const error = new Error("No data provided to parseData function");
+    console.error(error.stack);
+    throw error;
+  }
+
+  let lines = rawData.split("\n");
+
+  const parsedData: DataRow[] = [];
+
+  //special case, this is the header line
+  if (!lines[0].toLowerCase().includes("year: month")) {
+    throw "While parsing the first line of raw input, expected it to be the header line with 'year: month' inside";
+  }
+  const headerLineWithYearMonthRemoved = lines[0].replace(/year: month/i, "");
+  parsedData.push(headerLineWithYearMonthRemoved.trimLeft());
+
+  let trailingCharacter = null;
+  //deliberately skip first line, as that's the header line
+  for (let i = 1; i < lines.length; i++) {
+    //Example line is of format:
+    //Year: Month lifeInMonths L M S 1st 3rd 5th 15th 25th 50th 75th 85th 95th 97th 99th
+    //5: 1 61 -0.4681 18.2579 0.14295 13.4 14.2 14.6 15.8 16.6 18.3 20.2 21.3 23.4 24.3 26.2
+    const line = lines[i];
+    // if the line is just a newline, we want to keep it as is. This is to maintain any trailing newline in the
+    // raw data.
+    if (i == lines.length - 1) {
+      if (line.length === 1) {
+        trailingCharacter = line;
+      } else if (line.length == 0) {
+        //must have split on the newline
+        trailingCharacter = "\n";
+      }
+    }
+    if (line.length == 0) continue; // Skip empty lines so long as it's not the last line
+    const lineWithYearMonthRemoved = line.split(/[0-9]{1,2}:\s?[0-9]{1,2}/)[1];
+    parsedData.push(lineWithYearMonthRemoved.trimLeft());
+  }
+  return parsedData.join("\n") + (trailingCharacter ? trailingCharacter : "");
+}
+
 export function parseData(
   rawData,
   skipFirstLine = false,
-  removeYearMonth = false,
-  ignoreColumns: Array<number> = []
+  removeYearMonth = false
 ) {
   if (!rawData) {
     const error = new Error("No data provided to parseData function");
@@ -34,33 +94,13 @@ export function parseData(
     if (removeYearMonth) {
       relevantPartsOfLine = line.split(/[0-9]{1,2}:\s?[0-9]{1,2}/)[1].trim(); // Extract the rest of the line
     }
-    const partsWithMaybeExtraColumns = relevantPartsOfLine.split(/\s+/);
-    if (partsWithMaybeExtraColumns.length < 15)
+    const partsWithMaybeInsufficientColumns = relevantPartsOfLine.split(/\s+/);
+    if (partsWithMaybeInsufficientColumns.length < 15)
       console.warn(`Skipping line due to insufficient data: ${line}`);
     const parts: Array<string> = [];
-    ignoreColumns.forEach((columnIndexToIgnore, i) => {
-      if (columnIndexToIgnore < 0) {
-        ignoreColumns[i] += partsWithMaybeExtraColumns.length;
-      }
-    });
-    for (let j = 0; j < partsWithMaybeExtraColumns.length; j++) {
-      if (
-        ignoreColumns.includes(j) ||
-        ignoreColumns.includes(j + partsWithMaybeExtraColumns.length)
-      ) {
-        continue; // Skip ignored columns
-      }
-      const part = partsWithMaybeExtraColumns[j];
-      parts.push(part);
-    }
 
-    // if (ignoreColumns.length > 0) {
-    //   for (const col of ignoreColumns) {
-    //     if (col < parts.length) {
-    //       parts.splice(col, 1); // Remove the ignored column
-    //     }
-    //   }
-    // }
+    //TODO allow a percentages parameter to be passed into this function
+
     const lifeInMonths = parseInt(parts[0]);
     const L = parseFloat(parts[1]);
     const M = parseFloat(parts[2]);
@@ -80,7 +120,7 @@ export function parseData(
   return parsedData;
 }
 
-const percentiles = [1, 3, 5, 15, 25, 50, 75, 85, 95, 97, 99];
+// const percentiles = [1, 3, 5, 15, 25, 50, 75, 85, 95, 97, 99];
 
 export function calculatePercentile(
   ageInMonths,
