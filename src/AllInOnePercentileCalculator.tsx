da2389e4d45050ab182400977e5f4af6870238f3
ryan p <rjp-coder@gmail.com>
2025-05-19 12:23:39 +0100

    add styling for desktop

--- src/AllInOnePercentileCalculator.tsx
+++ src/AllInOnePercentileCalculator.tsx
@@ -9,9 +9,6 @@ import {
 import "./App.css";
 import { calculatePercentile, Result } from "./dataParser";
 
-const inputStyle = "border-2 border-gray-300 rounded-md mb-2";
-const labelStyle = "block text-lg font-bold mb-2";
-
 export const AllInOnePercentileCalculator = ({ allData }) => {
   const [age, setAge] = useState(""); // age in months
   const [weight, setWeight] = useState(""); //weight
@@ -23,7 +20,7 @@ export const AllInOnePercentileCalculator = ({ allData }) => {
 
   return (
     <section className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="pb-10">Percentile Calculator</h1>
+      <h1 className="pb-10 text-center">Percentile Calculator</h1>
       <div className="grid grid-cols-3">
         <div>
           <LabelledInput
@@ -34,7 +31,7 @@ export const AllInOnePercentileCalculator = ({ allData }) => {
             }}
             value={age}
           />
-          <p>{formatAge(interpretedAge.current)}</p>
+          <p className="text-center">{formatAge(interpretedAge.current)}</p>
           <LabelledInput
             label="Weight"
             onChange={(e) => setWeight(e.target.value)}
@@ -47,6 +44,7 @@ export const AllInOnePercentileCalculator = ({ allData }) => {
           />
           <button
             id="calculate_percentile"
+            className="w-1/1"
             onClick={() => {
               const result = { boys: {}, girls: {} };
               const measurements = { height, weight };
@@ -68,7 +66,7 @@ export const AllInOnePercentileCalculator = ({ allData }) => {
           </button>
         </div>
         <CombinedResultBox
-          className="grid grid-cols-2"
+          className="grid grid-cols-2 col-span-2 py-10"
           age={"" + formatAge(interpretedAge.current)}
           result={result}
         />
@@ -80,11 +78,14 @@ export const AllInOnePercentileCalculator = ({ allData }) => {
 const LabelledInput = ({ label, value, onChange }) => {
   return (
     <>
-      <label className={labelStyle} htmlFor={label}>
+      <label
+        className="block text-lg font-bold mb-2 text-center"
+        htmlFor={label}
+      >
         {label}
       </label>
       <input
-        className={inputStyle}
+        className="border-2 border-gray-300 rounded-md mb-2 text-center w-1/1"
         id={label}
         onChange={(e) => onChange(e)}
         value={value}
@@ -113,49 +114,43 @@ const CombinedResultBox = ({
 
   return (
     <div className={className}>
-      <p>With age of {age}</p>
-      <SingleResultBox
-        className="bg-blue-400 "
-        gender="Boys"
-        heightPercentile={boys.height.percentile}
-        heightError={boys.height.error}
-        weightPercentile={boys.weight.percentile}
-        weightError={boys.weight.error}
-      />
+      <p className="col-span-2 text-center text-2xl m-auto">Age {age}</p>
+      <SingleResultBox className="bg-blue-400 " gender="Boys" results={boys} />
       <SingleResultBox
         className="bg-pink-400 "
         gender="Girls"
-        heightPercentile={girls.height.percentile}
-        heightError={girls.height.error}
-        weightPercentile={girls.weight.percentile}
-        weightError={girls.weight.error}
+        results={girls}
       />
     </div>
   );
 };
 
-const SingleResultBox = ({
-  className,
-  gender,
-  heightPercentile,
-  weightPercentile,
-  heightError,
-  weightError,
-}) => {
+const SingleResultBox = ({ className, gender, results }) => {
+  console.log(results);
   return (
-    <div className={className}>
-      <h3>{gender}</h3>
-      {heightError || weightError ? (
+    <div className={className + " border-4 rounded-xl mx-2"}>
+      <h3 className="text-center">{gender}' percentile</h3>
+      {results.height.error || results.weight.error ? (
         <>
-          <p>{heightError}</p>
-          <p>{weightError}</p>
+          <p>{results.height.error}</p>
+          <p>{results.weight.error}</p>
         </>
       ) : (
         <>
-          <h4>{getOrdinalSuffix(heightPercentile)}</h4>
-          <span> for height</span>
-          <h4>{getOrdinalSuffix(weightPercentile)}</h4>
-          <span> for weight</span>
+          <h4 className="text-4xl text-center">
+            {Math.round(results.height.percentile)}
+            <span className="text-2xl">
+              {getOrdinalSuffix(Math.round(results.height.percentile))}
+            </span>
+            <span className="text-xs"> for height</span>
+          </h4>
+          <h4 className="text-4xl text-center">
+            {Math.round(results.weight.percentile)}
+            <span className="text-2xl">
+              {getOrdinalSuffix(Math.round(results.weight.percentile))}
+            </span>
+            <span className="text-xs"> for weight</span>
+          </h4>
         </>
       )}
     </div>
